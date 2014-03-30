83cbe48a285d8551f9c72ecde9ec9094ca95960a
Adrian L Lange <adrianlund@gmail.com>
2014-03-30 20:57:09 +0200

    Complete overhaul

--- scripts/options.js
+++ scripts/options.js
@@ -1,52 +1,32 @@
+var inputChange = function(){
+	localStorage.addItem(this.id, this.checked);
+};
+
 document.addEventListener('DOMContentLoaded', function(){
-	var interval = document.getElementById('interval');
-	interval.value = localStorage.getItem('interval');
+	[].forEach.call(this.querySelectorAll('input'), function(input){
+		input.addEventListener('change', inputChange);
+		input.checked = localStorage.getItem(input.id) === 'true';
+	});
 
-	var beta = document.getElementById('beta');
-	beta.checked = localStorage.getItem('beta') === 'true';
+	var interval = this.querySelector('span');
+	interval.innerText = localStorage.getItem('interval');
+	interval.addEventListener('mousewheel', function(event){
+		var value = +this.innerText;
 
-	var notifications = document.getElementById('notifications');
-	notifications.checked = localStorage.getItem('notifications') === 'true';
-});
+		if(event.wheelDelta > 0)
+			value += 1;
+		else
+			value -= 1;
 
-var interval;
-document.querySelector('#interval').addEventListener('input', function(x){
-	if(this.value < 1) this.value = 1;
-	if(this.value > 1440) this.value = 1440;
+		if(value < 1)
+			value = 1;
+		else if(value > 1440)
+			value = 1440;
 
-	localStorage.setItem('interval', this.value);
+		if(value != +this.innerText){
+			this.innerText = value;
 
-	var status = document.getElementsByTagName('span')[0];
-	status.innerHTML = 'Saved!';
-
-	clearTimeout(interval);
-	interval = setTimeout(function(){
-		status.innerHTML = '';
-	}, 750);
-});
-
-var beta;
-document.querySelector('#beta').addEventListener('change', function(){
-	localStorage.setItem('beta', this.checked);
-
-	var status = document.getElementsByTagName('span')[2];
-	status.innerHTML = 'Saved!';
-
-	clearTimeout(beta);
-	beta = setTimeout(function(){
-		status.innerHTML = '';
-	}, 750);
-});
-
-var notifications;
-document.querySelector('#notifications').addEventListener('change', function(){
-	localStorage.setItem('notifications', this.checked);
-
-	var status = document.getElementsByTagName('span')[3];
-	status.innerHTML = 'Saved!';
-
-	clearTimeout(notifications);
-	notifications = setTimeout(function(){
-		status.innerHTML = '';
-	}, 750);
+			localStorage.addItem('interval', value);
+		};
+	});
 });
